706f24888476d20e40f7d51aad786c236d79c6b0
adeyemitobiloba <tobilobaadeyemi400@gmail.com>
2024-07-06 23:01:44 +0100

    division.py

---
+++ user@.py
@@ -0,0 +1,32 @@
+correct_answer=(input("enter your score:"))
+english="what is a noun"
+mathematics="2/3"
+chemistry="define an atom"
+physics="state the SI unit of force"
+civic="define political apathy"
+biology="what is a cell"
+agriculture="give a valid reason to why Nigerians are suffering"
+geography="differenciate between atmosphere and climate"
+dataprocessing="list five ways of giving an information"
+economics="what is money"
+quiz_score=int("75")
+english=int("5")
+mathematics=int("8")
+chemistry=int("7")
+physics=int("4")
+civic=int("6")
+biology=int("2")
+agriculture=int("10")
+dataprocessing=int("15")
+geography=int("12")
+economics=int("3")
+if correct_answer=="english":
+    print("you are correct")
+elif correct_answer=="mathematics":
+    print("you are a genius")
+elif correct_answer=="agriculture":
+    print("you are good citizen")
+elif correct_answer=="quiz_score":
+    print("you are the winner")
+else:
+    print("you must be a scholar")    
